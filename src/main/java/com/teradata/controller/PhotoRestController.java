ec5eabc985e9d04126627e951ae7199c0b33de02
Daniel Song <xdtsong@gmail.com>
2017-03-17 15:35:25 -0700

    Refactor imports in PhotoRestController

--- src/main/java/com/teradata/controller/PhotoRestController.java
+++ src/main/java/com/teradata/controller/PhotoRestController.java
@@ -9,12 +9,16 @@ import com.teradata.resource.PhotoResource;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/{albumId}/photos")
