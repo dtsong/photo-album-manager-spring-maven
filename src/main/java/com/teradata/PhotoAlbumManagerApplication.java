d32235e4889840581b799880e0a1f3f8cc333b03
Daniel Song <xdtsong@gmail.com>
2017-03-17 11:56:27 -0700

    Initial Maven project

---
+++ src/main/java/com/teradata/PhotoAlbumManagerApplication.java
@@ -0,0 +1,12 @@
+package com.teradata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbumManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAlbumManagerApplication.class, args);
+	}
+}
