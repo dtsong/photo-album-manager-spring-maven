8293a9573fd5ce5ca49a4d88a33439445a520358
Daniel Song <xdtsong@gmail.com>
2017-03-17 12:07:46 -0700

    Implement AlbumNotFoundException

---
+++ src/main/java/com/teradata/exception/AlbumNotFoundException.java
@@ -0,0 +1,13 @@
+package com.teradata.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AlbumNotFoundException extends RuntimeException {
+
+    public AlbumNotFoundException(Long albumId) {
+        super("could not find album '" + albumId + "'.");
+    }
+}
