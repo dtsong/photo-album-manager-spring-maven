ac09819a428b03c3187cb83b03f31839c40c7ab6
Daniel Song <xdtsong@gmail.com>
2017-03-17 12:04:27 -0700

    Refactor to use Application.java for the SpringBootApplication

---
+++ src/main/java/com/teradata/Application.java
@@ -0,0 +1,34 @@
+package com.teradata;
+
+import com.teradata.model.Album;
+import com.teradata.model.Photo;
+import com.teradata.repository.AlbumRepository;
+import com.teradata.repository.PhotoRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(AlbumRepository albumRepository,
+						   PhotoRepository photoRepository) {
+		return (evt) -> Arrays.asList(
+				"Alaska Cruise, China Trip, Dank Memes, Wireframes".split(","))
+				.forEach(
+						a -> {
+							Album album = albumRepository.save(new Album(a));
+							photoRepository.save(new Photo(album,
+									"Photo1" + a, "http://placehold.it/600/92c952", "http://placehold.it/150/92c952"));
+							photoRepository.save(new Photo(album, "Photo2", "http://placehold.it/600/771796", "http://placehold.it/150/771796"));
+						});
+	}
+}
