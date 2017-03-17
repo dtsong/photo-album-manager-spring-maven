34fe858ed2b7e5aad0317aaa27fc8b8b374cda27
Daniel Song <xdtsong@gmail.com>
2017-03-17 12:46:39 -0700

    Refactor PhotoRestController to use the PhotoResource

--- src/main/java/com/teradata/controller/PhotoRestController.java
+++ src/main/java/com/teradata/controller/PhotoRestController.java
@@ -5,13 +5,17 @@ import com.teradata.model.Photo;
 import com.teradata.repository.AlbumRepository;
 import com.teradata.repository.PhotoRepository;
 import com.teradata.exception.AlbumNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.teradata.resource.PhotoResource;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/{albumId}/photos")
@@ -20,7 +24,6 @@ public class PhotoRestController {
     private final PhotoRepository photoRepository;
     private final AlbumRepository albumRepository;
 
-    @Autowired
     PhotoRestController(PhotoRepository photoRepository,
                         AlbumRepository albumRepository) {
         this.photoRepository = photoRepository;
@@ -30,36 +33,40 @@ public class PhotoRestController {
     @RequestMapping(method = RequestMethod.GET)
     Collection<Photo> readPhotos(@PathVariable String albumTitle) {
         this.validateAlbum(albumTitle);
-        return this.photoRepository.findByAlbumTitle(albumTitle);
+
+        List<PhotoResource> photoResourceList = photoRepository
+                                            .findByAlbumTitle(albumTitle).stream().map(PhotoResource::new)
+                                            .collect(Collectors.toList());
+
+        return new Resources<>(photoResourceList);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<?> add(@PathVariable String albumId, @RequestBody Photo input) {
         this.validateAlbum(albumId);
 
-        return this.albumRepository
+        return albumRepository
                 .findByTitle(albumId)
                 .map(album -> {
-                    Photo result = photoRepository.save(new Photo(album, input.title, input.url, input.thumbnailUrl));
+                    Photo photo = photoRepository.save(new Photo(album, input.title, input.url, input.thumbnailUrl));
 
-                    URI location = ServletUriComponentsBuilder
-                            .fromCurrentRequest().path("/{id}")
-                            .buildAndExpand(result.getId()).toUri();
+                    Link forOnePhoto = new PhotoResource(photo).getLink("self");
 
-                    return ResponseEntity.created(location).build();
+                    return ResponseEntity.created(URI.create(forOnePhoto.getHref())).build();
                 })
                 .orElse(ResponseEntity.noContent().build());
     }
 
     @RequestMapping(method = RequestMethod.GET, value="/{photoId}")
-    Photo readPhoto(@PathVariable String albumId, @PathVariable Long photoId) {
+    PhotoResource readPhoto(@PathVariable String albumId, @PathVariable Long photoId) {
         this.validateAlbum(albumId);
-        return this.photoRepository.findOne(photoId);
+        return new PhotoResource(this.photoRepository.findOne(photoId));
     }
 
 
     private void validateAlbum(String albumTitle) {
-        this.albumRepository.findByTitle(albumTitle).orElseThrow(
-                () -> new AlbumNotFoundException(albumTitle));
+        this.albumRepository
+                .findByTitle(albumTitle)
+                .orElseThrow(() -> new AlbumNotFoundException(albumTitle));
     }
 }
--- src/main/java/com/teradata/resource/PhotoResource.java
+++ src/main/java/com/teradata/resource/PhotoResource.java
@@ -5,6 +5,8 @@ import com.teradata.model.Photo;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
 public class PhotoResource extends ResourceSupport {
 
     private final Photo photo;
@@ -12,7 +14,7 @@ public class PhotoResource extends ResourceSupport {
     public PhotoResource(Photo photo) {
         String albumTitle = photo.getAlbum().getTitle();
         this.photo = photo;
-        this.add(new Link(photo.getUri(), "photo-uri"));
+        this.add(new Link(photo.getUrl(), "photo-url"));
         this.add(linkTo(PhotoRestController.class, albumTitle).withRel("photos"));
         this.add(linkTo(methodOn(PhotoRestController.class, albumTitle)
                         .readPhoto(albumTitle, photo.getId())).withSelfRel());
