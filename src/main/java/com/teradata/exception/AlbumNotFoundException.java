e5b11e06729d4991908fe2aa36fd71c9f40c29ce
Daniel Song <xdtsong@gmail.com>
2017-03-17 15:01:25 -0700

    refactor AlbumNotFoundException to be just a runtime exception

--- src/main/java/com/teradata/exception/AlbumNotFoundException.java
+++ src/main/java/com/teradata/exception/AlbumNotFoundException.java
@@ -1,10 +1,6 @@
 package com.teradata.exception;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AlbumNotFoundException extends RuntimeException {
 
     public AlbumNotFoundException(String albumId) {
