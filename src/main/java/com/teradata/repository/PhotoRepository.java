c5ce5a85fb8980d58a46e2cff57c2af8296150fe
Daniel Song <xdtsong@gmail.com>
2017-03-17 12:01:57 -0700

    Implement AlbumRepository to handle DB actions

---
+++ src/main/java/com/teradata/repository/PhotoRepository.java
@@ -0,0 +1,12 @@
+package com.teradata.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.teradata.model.Photo;
+
+import java.util.Collection;
+
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    Collection<Photo> findByAlbumId(Long id);
+}
