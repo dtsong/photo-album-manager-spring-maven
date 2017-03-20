136a6dc3fb7f70cb74b115e83f7deeb4eea5405e
Daniel Song <xdtsong@gmail.com>
2017-03-19 18:33:06 -0700

    Refactor json files to the resources/static/json directory

--- src/main/java/com/teradata/jsonreader/APIObjectMapper.java
+++ src/main/java/com/teradata/jsonreader/APIObjectMapper.java
@@ -16,14 +16,14 @@ public class APIObjectMapper {
 
     public Album readAlbumJsonWithObjectMapper() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        Album album = objectMapper.readValue(new File("/json/albums.json"), Album.class);
+        Album album = objectMapper.readValue(new File("/resources/static/json/albums.json"), Album.class);
         logger.info(album.toString());
         return album;
     }
 
     public Photo readPhotoJsonWithObjectMapper() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        Photo photo = objectMapper.readValue(new File("/json/photos.json"), Photo.class);
+        Photo photo = objectMapper.readValue(new File("/resources/static/json/photos.json"), Photo.class);
         logger.info(photo.toString());
         return photo;
     }
