3a940253a54b1337fdef43d4cf1d52405e917064
Daniel Song <xdtsong@gmail.com>
2017-03-19 21:34:35 -0700

    Add necessary spring annotations

--- src/test/java/com/teradata/jsonreader/APIObjectMapperTest.java
+++ src/test/java/com/teradata/jsonreader/APIObjectMapperTest.java
@@ -2,7 +2,15 @@ package com.teradata.jsonreader;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = APIObjectMapper.class)
+@WebAppConfiguration
 public class APIObjectMapperTest {
 
     @Test
