e5a4bb79b6befcc1a3b552051e8816d3b3948faf
Daniel Song <xdtsong@gmail.com>
2017-03-20 11:13:58 -0700

    Implement GET methods for Albums

--- src/main/java/com/teradata/controller/AlbumController.java
+++ src/main/java/com/teradata/controller/AlbumController.java
@@ -2,16 +2,36 @@ package com.teradata.controller;
 
 
 import com.teradata.dao.AlbumRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.teradata.model.Album;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
-@RequestMapping("/{userId}/albums")
+@RequestMapping("{username}/albums/")
 public class AlbumController {
 
     private final AlbumRepository albumRepository;
 
     AlbumController(AlbumRepository albumRepository) {
         this.albumRepository = albumRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Album> readPhotos(@PathVariable String username) {
+        this.validateUser(username);
+        return this.albumRepository.findByUsername(username);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/{albumId}")
+    Album readAlbum(@PathVariable String username, @PathVariable Long albumId) {
+        this.validateUser(username);
+        return this.albumRepository.findOne(albumId);
+    }
+
+    private void validateUser(String username) {
+        this.albumRepository.findByUsername(userId)
+                .orElseThrow(() -> new UserNotFoundException(username));
     }
 }
