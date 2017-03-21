3d9511182529ec8993cafd6da9b806daa566cd2c
Daniel Song <xdtsong@gmail.com>
2017-03-20 21:37:11 -0700

    Remove unneccessary annotations from Application.java

--- src/main/java/com/teradata/Application.java
+++ src/main/java/com/teradata/Application.java
@@ -1,17 +1,9 @@
 package com.teradata;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.teradata.dao"})
-@EntityScan(basePackages = {"com.teradata.model"})
-@EnableAutoConfiguration
-@EnableTransactionManagement
 public class Application {
 
 	public static void main(String[] args) {
