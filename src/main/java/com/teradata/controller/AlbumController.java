9f2dcf77b2f2ab15433b9a5f5c99efbfd9310e9a
Daniel Song <xdtsong@gmail.com>
2017-03-20 21:06:29 -0700

    Add informative comments to AlbumController

--- src/main/java/com/teradata/controller/AlbumController.java
+++ src/main/java/com/teradata/controller/AlbumController.java
@@ -17,26 +17,33 @@ public class AlbumController {
         this.albumRepository = albumRepository;
     }
 
+    /* CREATE - create an album
+     * REQUEST: POST /albums
+     */
     @RequestMapping(method = RequestMethod.POST)
     public Album create(@RequestBody Album album) {
         return this.albumRepository.save(album);
     }
 
+    /* READ - read all albums
+     * REQUEST: GET /albums
+     */
     @RequestMapping(method = RequestMethod.GET)
-    public Collection<Album> readAlbums() {
+    public Collection<Album> getAlbums() {
         return this.albumRepository.findAllByOrderByIdAsc();
     }
 
+    /* READ - read an album
+     * REQUEST: GET /albums/{id}
+     */
     @RequestMapping(method = RequestMethod.GET, value="/{id}")
-    public Album readAlbum(@PathVariable Long id) {
+    public Album getAlbumById(@PathVariable Long id) {
         return this.albumRepository.findOne(id);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value="{id}")
-    public void delete(@PathVariable Long id) {
-        this.albumRepository.delete(id);
-    }
-
+    /* UPDATE - update an album
+     * REQUEST: PUT /albums/{id}
+     */
     @RequestMapping(method = RequestMethod.PUT, value="{id}")
     public Album update(@PathVariable Long id, @RequestBody Album album) {
         Album update = this.albumRepository.findOne(id);
@@ -45,5 +52,15 @@ public class AlbumController {
         update.setUserId(album.getUserId());
         return this.albumRepository.save(update);
     }
+    
+    /* DELETE - delete an album
+     * REQUEST: DELETE /albums/{id}
+     */
+    @RequestMapping(method = RequestMethod.DELETE, value="{id}")
+    public void delete(@PathVariable Long id) {
+        this.albumRepository.delete(id);
+    }
+
+
 
 }
